930192670f96229a39fca2a4d1a1bac68f967130
PeterBrw <peter.giurgiu@gmail.com>
2020-07-15 14:01:01 +0300

    fixing-rotate

--- src/components/Pdf.js
+++ src/components/Pdf.js
@@ -1,21 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Document, Page } from "react-pdf";
 import "./Pdf.css";
 import VirtualList from "react-tiny-virtual-list";
 // import { Document, Page } from "react-pdf/dist/entry.webpack";
-import pdfdoc from "../docs/Health2020-Long.pdf";
+import pdfdoc from "../docs/PPTXtest.pptx.pdf";
 import { pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+const PAGE_HEIGHT = document.documentElement.clientHeight;
 
 export default function Pdf() {
     const [numPages, setNumPages] = useState(null);
     let [pageNumber, setPageNumber] = useState(1);
     const [array, setArray] = useState([]);
     let [zoom, setZoom] = useState(1);
-    // let [rotation, setRotation] = useState(0);
     let [rotateArr, setRotateArr] = useState([]);
-    // let id = 1;
-
+    
     function onDocumentLoadSuccess({ numPages }) {
         setArray(Array.apply(null, Array(numPages)).map((x, i) => i));
         setRotateArr(Array.apply(null, Array(numPages)).map(() => 0));
@@ -30,36 +29,56 @@ export default function Pdf() {
     };
 
     const rotateRight = () => {
-        rotateArr[pageNumber] += 90;
-    };
-
-    const rotateLeft = () => {
-        rotateArr[pageNumber] += 270;
-    };
-
-    const scroll = (e) => {
-        // console.log((e / 500 + 1).toFixed());
-        setPageNumber((e / 700 + 1).toFixed());
+        setRotateArr(
+            rotateArr.map((element, index) => {
+                if (index === pageNumber - 1) {
+                    console.log("hey");
+                    return element + 90;
+                }
+                return element;
+            })
+        );
         console.log(rotateArr);
     };
 
+    const rotateLeft = () => {
+        setRotateArr(
+            rotateArr.map((element, index) => {
+                if (index === pageNumber - 1) {
+                    console.log("not working");
+                    return element + 270;
+                }
+                return element;
+            })
+        );
+        console.log(rotateArr);
+    };
+
+    const scroll = (e) => {
+        setPageNumber((e / 700 + 1).toFixed());
+    };
+
     const handleChange = (e) => {
-        console.log("e: ", e.target.value);
-        if (e.target.value === "") {
-            setPageNumber(1);
-            console.log(pageNumber);
-            document
-                .querySelector(`div[data-page-number='1']`)
-                .scrollIntoView();
-        }
-        if (e.target.value >= 1 && e.target.value <= numPages) {
-            setPageNumber(parseInt(e.target.value));
-            console.log("pag num", pageNumber);
-            console.log(pageNumber, e.target.value);
-            document
-                .querySelector(`div[data-page-number='${e.target.value}']`)
-                .scrollIntoView();
-        }
+        setPageNumber(e.target.value);
+        // console.log("e: ", e.target.value);
+        // if (e.target.value === "") {
+        //     console.log("pageNumber: ", pageNumber);
+        //     document
+        //         .querySelector(`div[data-page-number='${pageNumber}']`)
+        //         .scrollIntoView();
+        // }
+        // document
+        //     .querySelector(`div[data-page-number='${e.target.value}']`)
+        //     .scrollIntoView();
+        // console.log(e.target.value);
+        // if (e.target.value >= 1 && e.target.value <= numPages) {
+        //     setPageNumber(parseInt(e.target.value));
+        //     console.log("pag num", pageNumber);
+        //     console.log(pageNumber, e.target.value);
+        //     document
+        //         .querySelector(`div[data-page-number='${e.target.value}']`)
+        //         .scrollIntoView();
+        // }
     };
 
     return (
@@ -82,9 +101,10 @@ export default function Pdf() {
                 <Document file={pdfdoc} onLoadSuccess={onDocumentLoadSuccess}>
                     <VirtualList
                         width="100%"
-                        height={500}
+                        height={700}
                         itemCount={array.length}
                         itemSize={700} // Also supports variable heights (array or function getter)
+                        scrollToIndex={pageNumber}
                         onScroll={scroll}
                         renderItem={({ index, style }) => (
                             <div key={index} style={style}>
@@ -92,16 +112,17 @@ export default function Pdf() {
                                     // id={id++}
                                     pageNumber={index + 1}
                                     scale={zoom}
-                                    rotate={rotateArr[pageNumber] % 360}
+                                    rotate={rotateArr[pageNumber - 1] % 360}
                                 />
                             </div>
                         )}
                     />
                 </Document>
-                <p>
-                    Page {pageNumber} of {numPages}
-                </p>
             </div>
         </div>
     );
 }
+
+// <p>
+// Page {pageNumber} of {numPages}
+// </p>
